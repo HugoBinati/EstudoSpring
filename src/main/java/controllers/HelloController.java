16800ea98827c7528275119f269ed4f381b87c32
HugoBinati <HugoBinati@hotmail.com>
2020-09-13 14:48:10 -0300

    first commit

---
+++ src/main/java/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="hello")
+public class HelloController {
+
+    @GetMapping(value="world")
+    public String helloWorld() {
+        return "hello world";
+    }
+}
